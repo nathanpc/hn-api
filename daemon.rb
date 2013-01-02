6f3938f14feb1d52e0a367579ba31d60970c0888
Nathan Campos <eeepc904@gmail.com>
2013-01-02 08:54:54 -0200

    Added a daemon

---
+++ daemon.rb
@@ -0,0 +1,9 @@
+require 'rubygems'
+require 'daemons'
+
+pwd  = File.dirname(File.expand_path(__FILE__))
+file = pwd + "app.rb"
+
+Daemons.run_proc("hn-api", :log_output => true) do
+	exec "ruby #{file}"
+end
