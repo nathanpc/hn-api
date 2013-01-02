5561c268cb0ea49b8d4915bae22ec13042f5d603
Nathan Campos <eeepc904@gmail.com>
2013-01-02 09:38:26 -0200

    Corrected the script path in the daemon script

--- daemon.rb
+++ daemon.rb
@@ -2,7 +2,7 @@ require 'rubygems'
 require 'daemons'
 
 pwd  = File.dirname(File.expand_path(__FILE__))
-file = pwd + "app.rb"
+file = File.join(pwd, "app.rb")
 
 Daemons.run_proc("hn-api", :log_output => true) do
 	exec "ruby #{file}"
